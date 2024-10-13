0472108a2043fc22376eb9371ed5dfea70168ca6
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 <scohail206@gmail.com>
2024-10-13 15:04:25 +0100

    clean modules 1

--- modules.py
+++ modules.py
@@ -2,7 +2,6 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
-
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms import Ollama
 from langchain.memory import ConversationBufferMemory
